62aea759ce936d162b75bdb7cce586e6d80a1a22
georgenardes <georgenardes@gmail.com>
2020-08-18 11:49:17 -0300

    Primeiro exercicio

---
+++ aula_1/exer_1/exem_proces.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+gain = 0.5
+sample_rate = 8000
+
+read_path = "../teste.pcm"
+with open(read_path, 'rb') as f:
+    buf = f.read()
+    data_i = np.frombuffer(buf, dtype='int16')
+    data_len = len(data_i)
+
+    # replica do arquivo lido para salvar o resultado
+    data_o = np.zeros_like(data_i)
+
+    for i in range(data_len):
+        data_o[i] = data_i[i] * gain
+
+    t = np.arange(0, data_len/sample_rate, 1 / sample_rate)
+
+    plt.plot(t, data_i[: len(t)], label="Input")
+    plt.plot(t, data_o[: len(t)], label="Output")
+    plt.legend()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.show()
+
+    ''' 
+    fig, axs = plt.subplots(2, 1)
+    axs[0].plot(t, data_i[: len(t)])
+    axs[1].plot(t, data_o[: len(t)])
+    plt.show()
+    '''
