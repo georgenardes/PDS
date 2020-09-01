fd9113f4fd950a306ee89fab1ec89a2d77691639
georgenardes <georgenardes@gmail.com>
2020-09-01 12:17:30 -0300

    salva sinais em arquivo pcm

--- aula_2/plota_sinais/plota_sinais.py
+++ aula_2/plota_sinais/plota_sinais.py
@@ -2,33 +2,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-sample_rate = 100
-index = int(sample_rate/2)
-t = np.arange(-index*0.125, index*0.125, 0.125)     # o passo deve ser um numero possivel de representar em bin
-
+sample_rate = 8000
+t = np.arange(-0.5, 0.5, 1/sample_rate)     # o passo deve ser um numero possivel de representar em bin
+tlen = len(t)
+t0 = int(tlen/2)
+print(tlen)
 
 ###############
 #   impulso
-sig_imp = np.zeros((sample_rate, 1))
-sig_imp[t == 0] = 1
+sig_imp = np.zeros((tlen, 1), dtype=np.int16)
+sig_imp[t == t[t0]] = 1
 
+with open(r'../sig_imp.pcm', 'wb') as f:
+    for d in sig_imp:
+        f.write(d)
 
 ###############
 #   degrau unitario
-sig_deg = np.zeros((sample_rate, 1))
+sig_deg = np.zeros((tlen, 1), dtype=np.int16)
 sig_deg[t > 0] = 1
+
+with open(r'../sig_deg.pcm', 'wb') as f:
+    for d in sig_deg:
+        f.write(d)
 
 ###############
 #   seno
-sig_sin = np.sin(t)
+f = 400
+sig_sin = (np.sin(2*np.pi*t*f)*1000).astype(np.int16)
+
+with open(r'../sig_sin.pcm', 'wb') as f:
+    for d in sig_sin:
+        f.write(d.astype(np.int16))
 
 ###############
 #   exp
 sig_exp = np.exp2(t)
 
+with open(r'../sig_exp.pcm', 'wb') as f:
+    for d in sig_exp:
+        f.write(d.astype(np.int16))
+
+###############
+#   plot
 fig, axs = plt.subplots(4, 1)
-axs[0].plot(t, sig_imp)
-axs[1].plot(t, sig_deg)
-axs[2].plot(t, sig_sin)
-axs[3].plot(t, sig_exp)
+axs[0].stem(t, sig_imp, "k-", "ko", "k-")
+axs[1].stem(t, sig_deg, "k-", "ko", "k-")
+axs[2].stem(t, sig_sin, "k-", "ko", "k-")
+axs[3].stem(t, sig_exp, "k-", "ko", "k-")
 plt.show()
